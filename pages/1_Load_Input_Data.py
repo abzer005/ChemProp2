0547fb00b39626be09eae70ae0fe9859c85a78de
Abzer <abzer.kelmina@gmail.com>
2023-11-28 14:34:07 +0100

    changes

---
+++ pages/1_Load_Input_Data.py
@@ -0,0 +1,51 @@
+# Import necessary libraries
+import streamlit as st
+from src.common import *
+from src.fileselection import *
+from src.cleanup import *
+import pandas as pd
+
+# Function definitions (e.g., load_from_gnps, load_example_data) are assumed to be defined in the imported modules
+
+# Page Setup
+page_setup()
+
+# Introduction Section
+st.markdown("""
+# Welcome to the Ocean Metabolome Portal
+Please select your method for data input below.
+""")
+
+# Input Selection Section
+input_method = st.selectbox("Select Input Method", 
+                            ["Manual Input", 
+                             "Automatic Input Retrieval (FBMN job ID)", 
+                             "Use Example Dataset"])
+
+# Manual Input Section
+if input_method == "Manual Input":
+    ft_file = st.file_uploader("Upload Feature Table", type=["csv", "xlsx", "txt"])
+    md_file = st.file_uploader("Upload Metadata", type=["csv", "xlsx", "txt"])
+    network_file = st.file_uploader("Upload Network Edge File from FBMN", type=["csv", "xlsx", "txt"])
+    annotation_file = st.file_uploader("Upload Annotation Information File (Optional)", type=["csv", "xlsx", "txt"])
+    # Add functionality to load and process these files
+
+# Automatic Input Retrieval Section
+elif input_method == "Automatic Input Retrieval (FBMN job ID)":
+    fbmn_job_id = st.text_input("Enter FBMN Job ID")
+    if fbmn_job_id:
+        # Functionality to retrieve and process files using the FBMN job ID
+
+# Example Dataset Section
+elif input_method == "Use Example Dataset":
+    example_fbmn_job_id = "3ebf9c7171fd43b0b5fc4c2cd9d16ed6"
+    st.write("Loading example dataset using FBMN job ID:", example_fbmn_job_id)
+    # Load data using the example FBMN job ID
+    # Example:
+    # ft, md, network_file, annotation_file = load_example_data(example_fbmn_job_id)
+
+# Data Processing and Visualization Section
+# Here you can add the functionality to process and visualize the data
+# This will depend on your specific project requirements
+
+# Ensure that the necessary functions for data loading and processing are defined in your imported modules or within this script
